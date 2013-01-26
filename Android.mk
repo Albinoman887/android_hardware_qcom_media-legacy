f04bbd530e645041e1b8621890410453ea5045c0
Dorian Snyder <dastin1015@gmail.com>
2013-01-26 13:00:12 -0800

    media-legacy: don't build if TARGET_QCOM_AUDIO_VARIANT is set
    Try building using qcom/media

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
 ifeq ($(BOARD_USES_LEGACY_QCOM),true)
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),)
     ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
         include $(call all-subdir-makefiles)
     endif
+endif
 endif
