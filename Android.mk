ccb60409421e437ee9edccadfb186ae8c796e77f
Dorian Snyder <dastin1015@gmail.com>
2013-01-26 20:35:16 -0800

    media: use only when needed
    use by setting BOARD_USES_LEGACY_MEDIA := true

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
 ifeq ($(BOARD_USES_LEGACY_QCOM),true)
+ifeq ($(BOARD_USES_LEGACY_MEDIA),true)
     ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
         include $(call all-subdir-makefiles)
     endif
+endif
 endif
