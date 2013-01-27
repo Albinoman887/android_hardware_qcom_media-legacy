3f65ab9551db1a5596aab140580b6a83c2b3d19d
Dorian Snyder <dastin1015@gmail.com>
2013-01-26 20:34:03 -0800

    Revert "media-legacy: don't build if TARGET_QCOM_AUDIO_VARIANT is set"
    This reverts commit f04bbd530e645041e1b8621890410453ea5045c0.

--- Android.mk
+++ Android.mk
@@ -1,7 +1,5 @@
 ifeq ($(BOARD_USES_LEGACY_QCOM),true)
-ifeq ($(TARGET_QCOM_AUDIO_VARIANT),)
     ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
         include $(call all-subdir-makefiles)
     endif
-endif
 endif
